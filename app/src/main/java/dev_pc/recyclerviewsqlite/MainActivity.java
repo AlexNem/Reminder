20852df46aa06dacf3813d6a530c256c861c1deb
TarasSagan <sagan0993@gmail.com>
2017-12-19 13:27:40 +0200

    My commit

--- app/src/main/java/dev_pc/recyclerviewsqlite/MainActivity.java
+++ app/src/main/java/dev_pc/recyclerviewsqlite/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private TasksFragment tasksFragment;
     private FragmentManager manager;
     private FragmentTransaction transaction;
-    String test = "tralyalya";
+    String test = "хуйляля";
     String d;
 
 
