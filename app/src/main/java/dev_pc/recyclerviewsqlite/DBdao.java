69ac50b41aa396c4ef52ebcc72797b76f150e0b5
kaban61 <kaban601@ukr.net>
2018-01-15 15:36:22 +0200

    add RecyclerView

---
+++ app/src/main/java/dev_pc/recyclerviewsqlite/DBdao.java
@@ -0,0 +1,52 @@
+package dev_pc.recyclerviewsqlite;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dev-pc on 13.01.2018.
+ */
+
+public class DBdao {
+
+    public List<AdapterZadach> getTaskList(Context context) {
+        List<AdapterZadach> list = new ArrayList<>();
+        String query = "SELECT * FROM " + MyDBHelper.TABLE_NAME;
+        MyDBHelper dbH = new MyDBHelper(context);
+        SQLiteDatabase db = dbH.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(cursor.getColumnIndex(MyDBHelper.KEY_ID));
+                String name = cursor.getString(cursor.getColumnIndex(MyDBHelper.KEY_NAME));
+                String inf = cursor.getString(cursor.getColumnIndex(MyDBHelper.KEY_INF));
+                list.add(new AdapterZadach(id, name, inf));
+            } while (cursor.moveToNext());
+        }
+        dbH.close();
+        return list;
+    }
+
+    public List<AdapterBirthday> getBirthdayList(Context context) {
+        List<AdapterBirthday> list = new ArrayList<>();
+        String query = "SELECT * FROM " + MyDBHelper.TABLE_BIRTHDAY;
+        MyDBHelper dbHelper = new MyDBHelper(context);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        Cursor cursor = database.rawQuery(query, null);
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(cursor.getColumnIndex(MyDBHelper.KEY_IDB));
+                String name = cursor.getString(cursor.getColumnIndex(MyDBHelper.KEY_BNAME));
+                String data = cursor.getString(cursor.getColumnIndex(MyDBHelper.KEY_DATA));
+                list.add(new AdapterBirthday(id, name, data));
+            } while (cursor.moveToNext());
+        }
+        dbHelper.close();
+        return list;
+    }
+
+}
---
+++ app/src/main/java/dev_pc/recyclerviewsqlite/fragments/birthdayFragment/MybirthdayRecyclerViewAdapter.java
@@ -0,0 +1,76 @@
+package dev_pc.recyclerviewsqlite.fragments.birthdayFragment;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import dev_pc.recyclerviewsqlite.AdapterBirthday;
+import dev_pc.recyclerviewsqlite.R;
+import dev_pc.recyclerviewsqlite.fragments.birthdayFragment.BirthdayFragment.OnListFragmentInteractionListener;
+
+import java.util.List;
+
+public class MybirthdayRecyclerViewAdapter extends RecyclerView.Adapter<MybirthdayRecyclerViewAdapter.ViewHolder> {
+
+    private final List<AdapterBirthday> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MybirthdayRecyclerViewAdapter(List<AdapterBirthday> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_birthday, parent, false);
+        Log.d("TAG", Integer.toString(mValues.size()));
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).getName());
+        holder.mContentView.setText(mValues.get(position).getData());
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction1(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public AdapterBirthday mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.name_b);
+            mContentView = (TextView) view.findViewById(R.id.data_b);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
